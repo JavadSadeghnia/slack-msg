f25c6b323d9e9b632fd0891ec1e5517f8593168c
JavadSadeghnia <108363507+JavadSadeghnia@users.noreply.github.com>
2022-10-13 22:01:41 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { WebClient, LogLevel } = require("@slack/web-api");
 const client = new WebClient("xoxb-4208929916324-4206536588322-bzN4PIMO44UPVM0LsIHz6tbj", {
-
   logLevel: LogLevel.DEBUG
 });
 
@@ -8,13 +7,11 @@ const channelId = "C0467PK7ZT8";
 const userId = "U0462BKP8QK";
 
 try {
-
   const result =  client.chat.postEphemeral({
     channel: channelId,
     user: userId,
     text: "This is a text message from Javascript!"
   });
-
   console.log(result);
 }
 catch (error) {
